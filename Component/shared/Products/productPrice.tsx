160b547979a2f9c5ab61c114dfee5f0c5c1b6d06
nazi sharifi <banonazisharifi@gmail.com>
2026-05-02 18:28:00 +0430

    add componennt price

--- Component/shared/Products/productPrice.tsx
+++ Component/shared/Products/productPrice.tsx
@@ -5,6 +5,8 @@ function ProductPrice({price}:{price:string}) {
   return (
     <div>
         <span className=' text-sm  align-super'>AFN</span>
+        <h1 className=" text-xl">{integer}</h1>
+         <span className=' text-sm  align-super'>{floate}</span>
     </div>
   )
 }
