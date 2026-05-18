1b562f44837c2c5eeefca7ad2f7648f1ba1810e2
nazi sharifi <banonazisharifi@gmail.com>
2026-05-18 23:20:32 +0430

    add form review

--- lib/action/review.action.ts
+++ lib/action/review.action.ts
@@ -3,3 +3,12 @@ export async function getReview(){
     const response = await data.json();
     return response;
 }
+export async function createReview(preves:any , formData:FormData){
+    const data = await fetch("http://localhost:8000/api/reviews" ,{
+        body: formData,
+        headers:{
+            accept : "application/json",
+        },
+        method: "POST"
+    });
+}
