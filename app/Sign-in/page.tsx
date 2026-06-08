b4712c2671da2c6db414b851cfe7c1e830f79ffd
nazi sharifi <banonazisharifi@gmail.com>
2026-06-08 22:39:53 +0430

    add create token

--- app/Sign-in/page.tsx
+++ app/Sign-in/page.tsx
@@ -17,12 +17,16 @@ import { useActionState } from "react"
 
 export default function CardDemo() {
   const [state , action] = useActionState(Login, {
+    data: "",
     success: false,
-    message: ""
   })
+  if(state.data !== "somting went wrong"){
+    localStorage.setItem("token", state.data);
+  }
   return (
     <div className=" w-full h-screen flex justify-center items-center">
            <Card className="w-full max-w-sm">
+        <form action={action}>
       <CardHeader>
         <CardTitle>Login to your account</CardTitle>
         <CardDescription>
@@ -30,17 +34,23 @@ export default function CardDemo() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form action={action}>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
                 id="email"
                 type="email"
-                name="name"
+                name="email"
                 placeholder="m@example.com"
                 required
               />
+              {state.errors && 
+              <div className=" text-red-500 text-sm">
+                {state.errors?.email.map((x:string)=>(
+                  <span>{x}</span>
+                ))}
+              </div>
+                }
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
@@ -53,8 +63,16 @@ export default function CardDemo() {
                 </a>
               </div>
               <Input id="password" name="password" type="password" required />
+               {state.errors && 
+              <div className=" text-red-500 text-sm">
+                {state.errors?.password.map((y:string )=>(
+                  <span>{y}</span>
+                ))}
+              </div>
+                }
             </div>
           </div>
+      </CardContent>
       <CardFooter className="flex-col gap-2 my-4">
         <Button type="submit" className="w-full">
           Login
@@ -64,7 +82,6 @@ export default function CardDemo() {
         </Button>
       </CardFooter>
         </form>
-      </CardContent>
     </Card>
     </div>
   )
--- lib/action/auth.action.ts
+++ lib/action/auth.action.ts
@@ -1,29 +1,18 @@
 "use server"
 export async function Login(prevState: unknown , formData:FormData){
     console.log(formData);
-    try{
 
-    const data = fetch('http://localhost:8000/api/auth' , {
+    const data = await fetch('http://localhost:8000/api/auth' , {
         body: formData,
         headers: {
              accept : "application/json",
         },
         method: "POST"
     });
-    const  response = (await data).json();
-    // console.log(response);
-    return {
-        success: true,
-        message: "you are logdin"
-    }
+    const  response = await data.json();
+    console.log(response);
+    return response
     // loveable
     // qaa.af
-}
-catch(err){
-     return {
-        success: false,
-        message: "you are not login somting went wrong"
-    }
-}
 
 }
