9a92262bec919b50a35164c8ec74f6c57286be11
nazi sharifi <banonazisharifi@gmail.com>
2026-06-22 21:36:41 +0430

    add ui dashboard

--- app/(root)/Dashbord/page.tsx
+++ app/(root)/Dashbord/page.tsx
@@ -1,16 +1,20 @@
+"use client"
 import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 import { LayoutDashboardIcon } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 
 function page() {
+    const [showText , setShowText] = useState(true)
   return (
     <div className='w-full p-2 flex'>
-        <div>
-            <Card>
+        <div className='max-w-3xl w-0'>
+            <Card className='rounded-none'>
                 <CardHeader>
-                    <CardTitle >
-                        <span><LayoutDashboardIcon/> Dashboard</span>
+                    <CardTitle className='flex items-center w-fit space-x-2' >
+                        <LayoutDashboardIcon size={24} />
+                        <span className={`${showText? "block" : "hidden"}`}> Dashboard</span>
                     </CardTitle>
+
                 </CardHeader>
             </Card>
         </div>
