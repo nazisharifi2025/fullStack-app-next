7c86a2e31c70afee05f68c7b592b21d387d1e4fb
nazi sharifi <banonazisharifi@gmail.com>
2026-06-22 21:31:58 +0430

    add ui dashboard

--- app/(root)/Dashbord/page.tsx
+++ app/(root)/Dashbord/page.tsx
@@ -1,8 +1,20 @@
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { LayoutDashboardIcon } from 'lucide-react'
 import React from 'react'
 
 function page() {
   return (
-    <div>page</div>
+    <div className='w-full p-2 flex'>
+        <div>
+            <Card>
+                <CardHeader>
+                    <CardTitle >
+                        <span><LayoutDashboardIcon/> Dashboard</span>
+                    </CardTitle>
+                </CardHeader>
+            </Card>
+        </div>
+    </div>
   )
 }
 
