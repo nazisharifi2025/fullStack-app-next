f2a73260beeb9c4c10f8100183e94dbdbc0d085c
nazi sharifi <banonazisharifi@gmail.com>
2026-07-06 11:24:36 +0430

    add store product form

--- Component/shared/dashboard/InsertProduct.tsx
+++ Component/shared/dashboard/InsertProduct.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 function InsertProduct() {
@@ -10,29 +11,39 @@ function InsertProduct() {
         <form action="" className='w-10/12 mx-auto grid grid-cols-2 gap-4'>
         <div className='flex flex-col gap-2'>
             <Label htmlFor='name' >Name</Label>
-            <Input type='text' className=' rounded-md' placeholder='Name' />
+            <Input type='text' className=' rounded-md' name='product_name' id='name' placeholder='Name' />
         </div>
         <div className='flex flex-col gap-2'>
             <Label htmlFor='name' >Price</Label>
-            <Input type='text' className=' rounded-md' placeholder='Name' />
+            <Input type='text' className=' rounded-md' name='product_price' id='price' placeholder='Name' />
         </div>
         <div className='flex flex-col gap-2'>
             <Label htmlFor='stock' >Stock</Label>
-            <Input type='text' className=' rounded-md' placeholder='stock' />
+            <Input type='text' className=' rounded-md' name='product_stock' id='stock' placeholder='stock' />
         </div>
         <div className='flex flex-col gap-2'>
             <Label htmlFor='brand' >Brand</Label>
-            <Input type='text' className=' rounded-md' placeholder='brand' />
+            <Input type='text' className=' rounded-md' name='product_brand' id='brand' placeholder='brand' />
         </div>
         <div className='flex flex-col gap-2'>
-            <Label htmlFor='name' >Expire Date</Label>
-            <Input type='text' className=' rounded-md' placeholder='Name' />
+            <Label htmlFor='expire_date' >Expire Date</Label>
+            <Input type='text' className=' rounded-md' name='product_expire_date' id='expire_date' placeholder='Expire_date' />
         </div>
         <div className='flex flex-col gap-2'>
-            <Label htmlFor='name' >Man Date</Label>
-            <Input type='text' className=' rounded-md' placeholder='Name' />
+            <Label htmlFor='man_date' >Man Date</Label>
+            <Input type='text' className=' rounded-md' name='product_man_date' id='mane_Date' placeholder='Man Date' />
         </div>
-        <Button className=' rounded-md w-fit'>Add Product</Button>
+        <div className='flex flex-col gap-2'>
+            <Label htmlFor='category' >Category</Label>
+            <Input type='text' className=' rounded-md' name='product_category' id='category' placeholder='Product Category' />
+        </div>
+        <div className='flex flex-col gap-2'>
+            <Label htmlFor='description' >Description</Label>
+            <Textarea className=' rounded-md resize-none w-full h-32' name='product_description' id='description' placeholder='Product description' />
+        </div>
+        <div className=' w-full flex justify-center my-4 col-span-2 items-center'>
+            <Button className=' rounded-md w-fit  mx-auto'>Add Product</Button>
+        </div>
         </form>
     </div>
   )
