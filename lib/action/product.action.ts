d95eaa70ac697bf4499e3c67f6acf60208f0337e
nazi sharifi <banonazisharifi@gmail.com>
2026-07-06 11:59:36 +0430

    add function store product form

--- lib/action/product.action.ts
+++ lib/action/product.action.ts
@@ -18,3 +18,36 @@ export async function fetchAllProduct(url:string){
     const response = await data.json();
     return response.allProduct;
 }
+export async function StoreProduct(prevState: unknown , formData:FormData){
+    const formdata = {
+        name: formData.get('product_name'),
+        price: formData.get('product_price'),
+        brand: formData.get('product_brand'),
+        category: formData.get('product_category'),
+        stock: formData.get('product_stock'),
+        description: formData.get('product_description'),
+    }
+    const data = await fetch('http://localhost:8000/api/dashboard/store-product',
+        {
+            headers:{
+                 accept : "application/json",
+                 contentType: "application/json"
+            },
+        }
+    );
+    const response = await data.json();
+    if(response === 'ok'){
+        console.log('data added');
+        return {
+            data: "data added seccessfully",
+            status: true
+        }
+    }
+    else{
+         return {
+            data: "Somting went Wrong",
+            status: false
+        }
+    }
+
+}
