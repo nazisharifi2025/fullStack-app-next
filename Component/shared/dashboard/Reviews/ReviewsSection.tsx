19e83457319adb1099f57c19075caa3bac489624
nazi sharifi <banonazisharifi@gmail.com>
2026-07-18 16:09:45 +0430

    add new component

--- Component/shared/dashboard/Reviews/ReviewsSection.tsx
+++ Component/shared/dashboard/Reviews/ReviewsSection.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function ReviewsSection() {
   return (
-    <div className="flex items-center gap-4 text-center ">ReviewsSection</div>
+    <div className="flex items-center gap-4 text-center  ">
+      <div className=' w-10/12 mx-auto'></div>
+    </div>
   )
 }
 
