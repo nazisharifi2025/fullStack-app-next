29476a0d13d2103412f6930d4d0a7c372bc56855
nazi sharifi <banonazisharifi@gmail.com>
2026-07-20 21:48:49 +0430

    add new table

--- Component/shared/dashboard/Reviews/ReviewsSection.tsx
+++ Component/shared/dashboard/Reviews/ReviewsSection.tsx
@@ -1,9 +1,21 @@
+import { Table, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import React from 'react'
 
 function ReviewsSection() {
   return (
     <div className="flex items-center gap-4 text-center  ">
-      <div className=' w-10/12 mx-auto'></div>
+      <div className=' w-10/12 mx-auto'>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Id</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>LastName</TableHead>
+            <TableHead>Email</TableHead>
+          </TableRow>
+        </TableHeader>
+      </Table>
+      </div>
     </div>
   )
 }
