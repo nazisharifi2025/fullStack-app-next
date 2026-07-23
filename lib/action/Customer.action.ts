e29e4a7a50198eb73c4b7647bb132382a4dd0084
nazi sharifi <banonazisharifi@gmail.com>
2026-07-23 12:34:36 +0430

    add get user

--- lib/action/Customer.action.ts
+++ lib/action/Customer.action.ts
@@ -1,4 +1,5 @@
 export async function getAllUser (){
     const data = await fetch('http://localhost:8000/api/dashboard/all-users');
     const response = await data.json();
+    return response;
 }
