9a5e97757a8801308243566d1ecc46955ed99089
nazi sharifi <banonazisharifi@gmail.com>
2026-07-30 22:21:06 +0430

    add new component for update

--- Component/Customer/UpdateUser.tsx
+++ Component/Customer/UpdateUser.tsx
@@ -1,8 +1,19 @@
+import { AlertDialog, AlertDialogContent, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function UpdateUser() {
   return (
-    <div className=' h-screen w-full flex justify-center items-center'>UpdateUser</div>
+    <div className=' h-screen w-full flex justify-center items-center'>
+      <AlertDialog>
+        <AlertDialogTrigger>
+          <Button>Update</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogTitle>Update User</AlertDialogTitle>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
   )
 }
 
